30ce952f065861551ed8da164c86fffc7e6221c9
RMcNally <halfalligator@gmail.com>
2017-03-07 20:54:45 -0500

    Create debugPrimo.js

---
+++ debugPrimo.js
@@ -0,0 +1,8 @@
+// Debug Primo Service Window
+//
+// ==UserScript==
+// @include       http://*
+// @include       https://*
+// ==/UserScript==
+
+javascript:function o(){window.open(location.href+'&debug=true',"Debug")};o()
